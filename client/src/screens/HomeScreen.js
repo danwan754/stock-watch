28883ee5eb172cbbf93fc8e72173245884e90e14
danwan754 <danwan754@gmail.com>
2021-09-13 22:48:11 -0700

    Clean up commented code

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,10 +1,4 @@
-import React, { useEffect } from 'react';
-
 import '../css/screens/HomeScreen.css';
-
-// const backgroundStyle = {
-//     backgroundImage: `linear-gradient(rgba(255,255,255,0.0), rgba(255,255,255,0.0)), url('/home-stock-market.jpg')`
-// }
 
 function HomeScreen(props) {
 
@@ -12,7 +6,6 @@ function HomeScreen(props) {
         <div className="home-screen-container">
             <div 
                 className="home-banner"
-                // style={ backgroundStyle }
             >
                 S t o c k &nbsp; W a t c h
             </div>
