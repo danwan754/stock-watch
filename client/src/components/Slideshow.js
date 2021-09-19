e531acecff9d1d6b1a14a535221563de9a21f6a4
danwan754 <danwan754@gmail.com>
2021-09-18 21:38:41 -0700

    Add slideshow in home page

---
+++ client/src/components/Slideshow.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import '../css/components/Slideshow.css';
+
+const slides = ["/slide-lists.png", "/slide-quote.png", "/slide-edit.png"];
+const delay = 3000;
+
+function Slideshow() {
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  const resetTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  const startTimeout = () => {
+    timeoutRef.current = setTimeout(
+        () =>
+          setIndex((prevIndex) =>
+            prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+          ),
+        delay
+      );
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    startTimeout();
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {slides.map((slide, index) => (
+            <img
+                key={index}
+                className="slide-image slide"
+                src={slide}
+                alt="slide image"
+                onMouseOver={resetTimeout}
+                onMouseOut={startTimeout}
+            />
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {slides.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Slideshow;
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,14 +1,11 @@
+import Slideshow from '../components/Slideshow';
 import '../css/screens/HomeScreen.css';
 
 function HomeScreen(props) {
 
     return (
         <div className="home-screen-container">
-            <div 
-                className="home-banner"
-            >
-                S t o c k &nbsp; W a t c h
-            </div>
+            <Slideshow />
         </div>
     );
 }
